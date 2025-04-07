2e95ea2e79d3b6d4ee8654ba7533d403dedc4356
darthspectrum <muradianator@gmail.com>
2025-04-07 18:09:27 +0400

    A generic code example in Scala

--- macros/Prefix in Scala/Main.scala
+++ macros/Prefix in Scala/Main.scala
@@ -1,8 +1,21 @@
 import Prefix.*
 
+def logPayment(payment: String): String = {
+  val currency  = s"${payment(0)}${payment(1)}${payment(2)}"
+  val amount    = payment.drop(3).toDouble
+  val timestamp = System.currentTimeMillis()
+  s"$timestamp: $amount paid in $currency"
+}
+
 extension (inline ctx: StringContext) inline def prefix(): Int = Prefix.prefix(ctx)
 
 @main def run(): Unit =
+  println("Generic code example")
+  println(logPayment("USD123.45"))
+  println(logPayment("EUR678.90"))
+  println
+
+  println("Macro code example")
   println(prefix"+ 2 5 9")
   println(prefix"* 3 4 2")
 
